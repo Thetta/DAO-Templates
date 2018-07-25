526c1375206807eb706230add1e9dd880c43fe97
Ryzhak Vladimir <ryzhak.vladimir@gmail.com>
2018-07-25 13:37:42 +0300

    fixed typo in truffle config

--- truffle.js
+++ truffle.js
@@ -18,19 +18,19 @@ module.exports = {
 			gasPrice: 0x01,
 		},
 		main: {
-			provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
 			network_id: 1
 		},
 		ropsten: {
-			provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
 			network_id: 3
 		},
 		kovan: {
-			provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
 			network_id: 42
 		},
 		rinkeby: {
-			provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+			provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
 			network_id: 4
 		}
     }
